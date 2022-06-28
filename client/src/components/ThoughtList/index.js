f5437beed7b3257de22c9652d8c631e1dbc446b0
Michael Kendrick <michael@locumsmart.net>
2022-06-28 18:24:21 -0500

    getting help

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ThoughtList = ({ thoughts, title }) => {
-  if (!thoughts.length) {
+  if (!thoughts || !thoughts.length) {
     return <h3>No Thoughts Yet</h3>;
   }
 
@@ -12,15 +13,23 @@ const ThoughtList = ({ thoughts, title }) => {
         thoughts.map((thought) => (
           <div key={thought._id} className="card mb-3">
             <p className="card-header">
-              {thought.username}
+              <Link
+                to={`/profile/${thought.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {thought.username}
+              </Link>
               thought on {thought.createdAt}
             </p>
             <div className="card-body">
-              <p>{thought.thoughtText}</p>
-              <p className="mb-0">
-                Reactions: {thought.reactionCount} || Click to{" "}
-                {thought.reactionCount ? "see" : "start"} the discussion!
-              </p>
+              <Link to={`/thought/${thought._id}`}>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionCount} || Click to{" "}
+                  {thought.reactionCount ? "see" : "start"} the discussion!
+                </p>
+              </Link>
             </div>
           </div>
         ))}
